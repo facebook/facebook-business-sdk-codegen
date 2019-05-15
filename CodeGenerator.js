935e21f3fdca9eae028671c7881588e56bf1f9cb
Cody Winton <cody.t.winton@gmail.com>
2019-05-15 16:43:40 -0700

    Modernized CodeGenerator.js

--- CodeGenerator.js
+++ CodeGenerator.js
@@ -4,35 +4,26 @@
  * @format
  */
 
+'use strict';
+
 const minimist = require('minimist');
-const fs = require('fs');
-var path = require('path');
-const Utils = require('./common/Utils');
+
 const Procedure = require('./common/Procedure');
-
+const Utils = require('./common/Utils');
 const SpecFileLoader = require('./loaders/SpecFileLoader');
-
 const AutoAddIdFieldForRootNodeProcessor = require('./processors/AutoAddIdFieldForRootNodeProcessor');
-const ReferenceProcessor = require('./processors/ReferenceProcessor');
-const NormalizationProcessor = require('./processors/NormalizationProcessor');
-const SpecOverridingProcessor = require('./processors/SpecOverridingProcessor');
-const NodeEndpointHackProcessor = require('./processors/NodeEndpointHackProcessor');
-const NamingConventionProcessor = require('./processors/NamingConventionProcessor');
 const CreationEndpointHackProcessor = require('./processors/CreationEndpointHackProcessor');
-const LanguageSpecificProcessor = require('./processors/LanguageSpecificProcessor');
 const FlaggingProcessor = require('./processors/FlaggingProcessor');
-
-const MustacheRenderer = require('./renderers/MustacheRenderer');
+const LanguageSpecificProcessor = require('./processors/LanguageSpecificProcessor');
+const NamingConventionProcessor = require('./processors/NamingConventionProcessor');
+const NodeEndpointHackProcessor = require('./processors/NodeEndpointHackProcessor');
+const NormalizationProcessor = require('./processors/NormalizationProcessor');
+const ReferenceProcessor = require('./processors/ReferenceProcessor');
+const SpecOverridingProcessor = require('./processors/SpecOverridingProcessor');
 const DebugJsonRenderer = require('./renderers/DebugJsonRenderer');
-const args = minimist(process.argv.slice(2));
+const MustacheRenderer = require('./renderers/MustacheRenderer');
 
-const language = args._[0];
-Utils.validateLanguage(language);
-const version = args.v || Utils.loadDefaultVersion();
-const outputDir = args.o || 'sdk/servers/' + language + '/release';
-const cleandir = args.c ? args.c.split(',') : [];
-
-var procedure = new Procedure({
+const procedure = new Procedure({
   loader: SpecFileLoader,
   processors: [
     SpecOverridingProcessor,
@@ -47,5 +38,19 @@ var procedure = new Procedure({
   ],
   renderers: [MustacheRenderer, DebugJsonRenderer],
 });
+
+const args = minimist(process.argv.slice(2));
+
+const language = args._[0];
+Utils.validateLanguage(language);
+
+/** @type {string} */
+const version = args.v || Utils.loadDefaultVersion();
+
+/** @type {string} */
+const outputDir = args.o || 'sdk/servers/' + language + '/release';
+
+/** @type {string[]} */
+const cleandir = args.c ? args.c.split(',') : [];
 
 procedure.run(version, language, outputDir, cleandir);
