1e34957434a28c12d201ddb0a44d68810a34f6f0
Cody Winton <cody.t.winton@gmail.com>
2019-05-15 18:23:20 -0700

    Modernized MustacheRenderer.js

--- renderers/MustacheRenderer.js
+++ renderers/MustacheRenderer.js
@@ -4,7 +4,13 @@
  * @format
  */
 
-const fs = require('fs-extra');
+/**
+ * @typedef { import("../common/typedefs").Renderer } Renderer
+ */
+
+'use strict';
+
+const fs_extra = require('fs-extra');
 const mustache = require('mustache');
 const path = require('path');
 
@@ -12,89 +18,86 @@ const codeGenLanguages = require('../processors/CodeGenLanguages');
 const utils = require('./Utils');
 const commonUtils = require('../common/Utils');
 
-const MustacheRenderer = {};
+/**
+ * @type {Renderer}
+ */
+const MustacheRenderer = {
+  render(specs, language, version, outputDir, cleandir) {
+    const APISpecs = specs['APISpecs'];
+    const SDKConfig = specs['SDKConfig'];
 
-MustacheRenderer.render = function(
-  specs,
-  language,
-  version,
-  outputDir,
-  cleandir,
-) {
-  const APISpecs = specs['APISpecs'];
-  const SDKConfig = specs['SDKConfig'];
+    const codegenRootDir = path.resolve(__dirname, '..');
+    const templateDir = path.resolve(codegenRootDir, 'templates', language);
+    const sdkRootPath = path.resolve(outputDir);
 
-  var codegenRootDir = path.resolve(__dirname, '..');
-  var templateDir = path.resolve(codegenRootDir, 'templates', language);
-  var sdkRootPath = path.resolve(outputDir);
+    // load the mustache templates
+    const loadedTemplates = utils.loadTemplates(templateDir);
+    const mainTemplates = loadedTemplates.mainTemplates;
+    const partialTemplates = loadedTemplates.partialTemplates;
+    const versionedTemplates = loadedTemplates.versionedTemplates;
+    const filesNeedCopy = loadedTemplates.filesNeedCopy;
 
-  // load the mustache templates
-  var loadedTemplates = utils.loadTemplates(templateDir);
-  var mainTemplates = loadedTemplates.mainTemplates;
-  var partialTemplates = loadedTemplates.partialTemplates;
-  var versionedTemplates = loadedTemplates.versionedTemplates;
-  var filesNeedCopy = loadedTemplates.filesNeedCopy;
-
-  // clean up the folder
-  if (
-    cleandir === undefined ||
-    cleandir.length == 0 ||
-    !fs.existsSync(sdkRootPath)
-  ) {
-    utils.removeRecursiveSync(sdkRootPath);
-    utils.mkdirsSync(sdkRootPath);
-  } else {
-    for (var d of cleandir) {
-      let tmp = path.resolve(sdkRootPath, d);
-      console.log(tmp);
-      utils.removeRecursiveSync(tmp);
-      utils.mkdirsSync(tmp);
+    // clean up the folder
+    if (
+      cleandir === undefined ||
+      cleandir.length == 0 ||
+      !fs_extra.existsSync(sdkRootPath)
+    ) {
+      utils.removeRecursiveSync(sdkRootPath);
+      utils.mkdirsSync(sdkRootPath);
+    } else {
+      for (const d of cleandir) {
+        const tmp = path.resolve(sdkRootPath, d);
+        console.log(tmp);
+        utils.removeRecursiveSync(tmp);
+        utils.mkdirsSync(tmp);
+      }
     }
-  }
 
-  // Copy the common folder
-  console.log('Generating ' + language + ' SDK in ' + outputDir + '...');
+    // Copy the common folder
+    console.log('Generating ' + language + ' SDK in ' + outputDir + '...');
 
-  // @fb-only
-  // @fb-only
+    // @fb-only
+    // @fb-only
 
-  // Generate code with mustache templates
-  for (var nodeName in APISpecs) {
-    var APIClsSpec = APISpecs[nodeName];
-    utils.fillMainTemplates(
-      mainTemplates,
-      partialTemplates,
-      APIClsSpec,
-      language,
-      codeGenLanguages,
-      sdkRootPath,
-    );
-  }
+    // Generate code with mustache templates
+    for (const nodeName in APISpecs) {
+      const APIClsSpec = APISpecs[nodeName];
+      utils.fillMainTemplates(
+        mainTemplates,
+        partialTemplates,
+        APIClsSpec,
+        language,
+        codeGenLanguages,
+        sdkRootPath,
+      );
+    }
 
-  const codeGenFileDepreciationSign = commonUtils.codeGenFileDepreciationSign;
-  const apiSpecArray = [];
-  for (var nodeName in APISpecs) {
-    var APIClsSpec = APISpecs[nodeName];
-    apiSpecArray.push(APIClsSpec);
-  }
-  SDKConfig.api_specs = apiSpecArray;
-  versionedTemplates.forEach(function(value) {
-    var fileContent = mustache.render(value.content, SDKConfig, {});
-    if (fileContent.trim().indexOf(codeGenFileDepreciationSign + '\n')) {
-      var destDir = sdkRootPath + value.dir;
+    const depreciationSign = commonUtils.codeGenFileDepreciationSign;
+    const apiSpecArray = [];
+    for (const nodeName in APISpecs) {
+      const APIClsSpec = APISpecs[nodeName];
+      apiSpecArray.push(APIClsSpec);
+    }
+    SDKConfig.api_specs = apiSpecArray;
+    versionedTemplates.forEach(value => {
+      const fileContent = mustache.render(value.content, SDKConfig, {});
+      if (fileContent.trim().indexOf(depreciationSign + '\n')) {
+        const destDir = sdkRootPath + value.dir;
+        utils.mkdirsSync(destDir);
+        fs_extra.writeFileSync(destDir + value.file, fileContent);
+      }
+    });
+
+    // Copy static files
+    filesNeedCopy.forEach(value => {
+      const destDir = sdkRootPath + value.dir;
       utils.mkdirsSync(destDir);
-      fs.writeFileSync(destDir + value.file, fileContent);
-    }
-  });
 
-  // Copy static files
-  filesNeedCopy.forEach(function(value) {
-    var destDir = sdkRootPath + value.dir;
-    utils.mkdirsSync(destDir);
-
-    fs.copy(templateDir + value.dir + value.file, destDir + value.file);
-  });
-  console.log(language + ' SDK generated in ' + outputDir);
+      fs_extra.copy(templateDir + value.dir + value.file, destDir + value.file);
+    });
+    console.log(language + ' SDK generated in ' + outputDir);
+  },
 };
 
 module.exports = MustacheRenderer;
