24a30d2835157b6fb514f5906ebc0d3f3d96ad40
Jing Ping <jingping@fb.com>
2019-10-27 11:47:20 -0700

    Fix ruby open source requirements.
    Summary: As title
    
    Reviewed By: joesus
    
    Differential Revision: D18129792
    
    fbshipit-source-id: 3f082da3

--- templates/ruby/lib/facebook_ads/crash_logger.rb
+++ templates/ruby/lib/facebook_ads/crash_logger.rb
@@ -1,3 +1,21 @@
+# Copyright (c) 2017-present, Facebook, Inc. All rights reserved.
+#
+# You are hereby granted a non-exclusive, worldwide, royalty-free license to use,
+# copy, modify, and distribute this software in source code or binary form for use
+# in connection with the web services and APIs provided by Facebook.
+#
+# As with any software that integrates with the Facebook platform, your use of
+# this software is subject to the Facebook Platform Policy
+# [http://developers.facebook.com/policy/]. This copyright notice shall be
+# included in all copies or substantial portions of the software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 require 'facebook_ads/api_request'
 require 'facebook_ads/api_response'
 
@@ -10,9 +28,13 @@ module FacebookAds
 
       def logCrash
         error = $!
-
         if is_facebook_error(error)
+          app_id = get_app_id
           # TODO: Determine what platform information should be logged
+          if app_id.nil?
+            puts "Unable to log the crash: We're missing an app id."
+            return
+          end
           params = {
             'bizsdk_crash_report': {
               'reason': formatted_reason(error),
@@ -22,7 +44,7 @@ module FacebookAds
           }
           APIRequest.new(
             :post,
-            '/instruments',
+            "#{app_id}/instruments",
             session: Session.anonymous_session,
             params: params,
             options: {} # batch options, not necessary for now
@@ -53,6 +75,32 @@ module FacebookAds
           reason = 'SDK Error'
         end
         reason
+      end
+
+      def get_app_id
+        params = {
+          'input_token': Session.default_session.access_token,
+          'fields': 'app_id'
+        }
+        begin
+          APIRequest.new(
+            :get,
+            '/debug_token',
+            session: Session.default_session,
+            params: params,
+            options: {} # batch options, not necessary for now
+          ).execute do |response|
+            if (response.result.has_key?('data') &&
+              response.result['data'].has_key?('app_id')
+            )
+              response.result['data']['app_id']
+            else
+              nil
+            end
+          end
+        rescue
+          nil
+        end
       end
 
       public
