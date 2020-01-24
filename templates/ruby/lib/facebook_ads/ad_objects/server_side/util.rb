2adaf1259bda0e8b641546287dc5a93a20a72674
Gaurav Saxena <gvs@fb.com>
2020-01-24 12:25:31 -0800

    Integrate with countries and money to validate country and currency codes.
    Summary:
    Add dependency on countries and money and use those to validate the passed values
    
    Adding dependencies on -
    
    https://github.com/hexorx/countries
    https://github.com/RubyMoney/money
    
    Reviewed By: vinothsa, mynameisram
    
    Differential Revision: D19526636
    
    fbshipit-source-id: aca1ae31

--- templates/ruby/lib/facebook_ads/ad_objects/server_side/util.rb
+++ templates/ruby/lib/facebook_ads/ad_objects/server_side/util.rb
@@ -17,6 +17,8 @@
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 require 'digest'
+require 'countries'
+require 'money'
 
 module FacebookAds
 	module ServerSide
@@ -100,8 +102,8 @@ module FacebookAds
 
 			# Replace unwanted characters and retain only alpha characters bounded for ISO code.
 			country = country.gsub(/[^a-z]/,'')
-
-			if country.length != 2
+			iso_country = ISO3166::Country.search(country)
+			if iso_country == nil
 				raise ArgumentError, "Invalid format for country:'" + country + "'.Please follow ISO 2-letter ISO 3166-1 standard for representing country. eg: us"
 			end
 
@@ -112,33 +114,34 @@ module FacebookAds
     def self.normalize_city(city)
 
       # Remove commonly occuring characters from city name.
-			city = city.gsub(/[0-9.\s\-()]/,'')
+		city = city.gsub(/[0-9.\s\-()]/,'')
 
-			return city
+		return city
+	end
+
+	# Normalizes the given currency code and returns acceptable hashed currency ISO code
+	def self.normalize_currency(currency)
+
+		# Retain only alpha characters bounded for ISO code.
+		currency = currency.gsub(/[^a-z]/,'')
+
+		iso_currency = Money::Currency.find(currency)
+		if iso_currency == nil
+			raise ArgumentError, "Invalid format for currency:'" + currency + "'.Please follow ISO 3-letter ISO 4217 standard for representing currency. Eg: usd"
 		end
 
-		# Normalizes the given currency code and returns acceptable hashed currency ISO code
-		def self.normalize_currency(currency)
+		return currency;
+	end
 
-			# Retain only alpha characters bounded for ISO code.
-			currency = currency.gsub(/[^a-z]/,'')
+	# Normalizes the given email and returns acceptable hashed email value
+	def self.normalize_email(email)
 
-			if currency.length != 3
-				raise ArgumentError, "Invalid format for currency:'" + currency + "'.Please follow ISO 3-letter ISO 4217 standard for representing currency. Eg: usd"
-			end
-
-			return currency;
+		if EMAIL_REGEX.match(email) == nil
+			return ArgumentError, "Invalid email format for the passed email:' + email + '.Please check the passed email format."
 		end
 
-		# Normalizes the given email and returns acceptable hashed email value
-		def self.normalize_email(email)
-
-			if EMAIL_REGEX.match(email) == nil
-				return ArgumentError, "Invalid email format for the passed email:' + email + '.Please check the passed email format."
-			end
-
-			return email
-		end
+		return email
+	end
 
 		# Normalizes the given gender and returns acceptable hashed gender value
     def self.normalize_gender(gender)
